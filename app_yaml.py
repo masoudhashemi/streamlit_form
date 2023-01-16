02efe7a7853e3d41bce38c9c767d6f4ec0f5204b
Masoud Hashemi <smasoud.hashemi@gmail.com>
2023-01-16 15:18:12 -0500

    Add requirements

--- app_yaml.py
+++ app_yaml.py
@@ -1,8 +1,9 @@
 import csv
-import streamlit as st
 from pathlib import Path
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+import streamlit as st
 import yaml
 
 with open("info.yaml", "r") as stream:
